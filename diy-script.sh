fa9ad3dff51289ac9ce08126a0cc83245ef0a9cd
NERD <168670760+zhiern@users.noreply.github.com>
2025-07-28 01:50:26 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -86,7 +86,7 @@ chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
 date_version=$(date +"%y.%m.%d")
 orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 sed -i "s/${orig_version}/R${date_version} by OPPEN321/g" package/lean/default-settings/files/zzz-default-settings
-sed -i "s|echo \"DISTRIB_DESCRIPTION='LEDE '|\"echo \"DISTRIB_DESCRIPTION='ZeroWrt 标准版 @ '|\"g" package/lean/default-settings/files/zzz-default-settings
+sed -i 's/LEDE/ZeroWrt 标准版 @/g' package/lean/default-settings/files/zzz-default-settings
 
 # 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci/Makefile
